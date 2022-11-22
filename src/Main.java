f3ab4d050a32b17efff452ee7a0b19c1a24e3b1f
DavidP1996erea <david.perea@iesnervion.es>
2022-11-22 19:08:18 +0100

    sdf

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.concurrent.Semaphore;
 
    public class Main implements Runnable {
         // Se crea el objeto tipo Semaphore, y se le pone un límite de 4
-        Semaphore charcutería = new Semaphore(4);
+        Semaphore charcutería = new Semaphore(2);
        Semaphore carnicería = new Semaphore(4);
         public static void main(String[] args)  {
 
@@ -23,20 +23,34 @@ import java.util.concurrent.Semaphore;
         @Override
         public void run() {
 
+            boolean esAtendidoCarniceria=false;
+            boolean esAtendidoCharcuteria=false;
             try {
-                carnicería.acquire();
-                System.out.println(Thread.currentThread().getName() + "Está siendo atendido en la Carnicería");
-                Thread.sleep((int) (Math.random()*10000));
-                System.out.println(Thread.currentThread().getName() + "Ha terminado de ser atendidoen la Carnicería");
-                carnicería.release();
 
-                charcutería.acquire();
-                System.out.println(Thread.currentThread().getName() + "Está siendo atendido en la Charcutería");
-                Thread.sleep((int) (Math.random()*10000));
-                System.out.println(Thread.currentThread().getName() + "Ha terminado de ser atendido en la Charcutería");
-                charcutería.release();
+                while (carnicería.availablePermits() != 0 ||  charcutería.availablePermits() != 0) {
 
+                    if(!esAtendidoCarniceria) {
+                        carnicería.acquire();
+                        System.out.println(Thread.currentThread().getName() + "Está siendo atendido en la Carnicería");
+                        Thread.sleep((int) (Math.random() * 10000));
+                        System.out.println(Thread.currentThread().getName() + "Ha terminado de ser atendido en la Carnicería");
+                        carnicería.release();
 
+                        esAtendidoCarniceria=true;
+                    }
+                    if(!esAtendidoCharcuteria) {
+                        charcutería.acquire();
+                        System.out.println(Thread.currentThread().getName() + "Está siendo atendido en la Charcutería");
+                        Thread.sleep((int) (Math.random() * 10000));
+                        System.out.println(Thread.currentThread().getName() + "Ha terminado de ser atendido en la Charcutería");
+                        charcutería.release();
+                        esAtendidoCharcuteria=true;
+                    }
+
+                    if(esAtendidoCarniceria && esAtendidoCharcuteria){
+                        Thread.interrupted();
+                    }
+                }
 
 
             } catch (InterruptedException e) {
